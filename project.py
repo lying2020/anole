59bb4605a30a58bb08733969da1021e980035118
lying2020 <824262075@qq.com>
2026-03-15 22:28:38 +0800

    update anole model path

--- project.py
+++ project.py
@@ -14,5 +14,6 @@ amber_data_path = "/home/liying/Documents/MLLMBenchMark/AMBER/data"
 llava_v15_7b_path = "/home/liying/Documents/llava-v1.5-7b"
 clip_vit_large_patch14_path = "/home/liying/Documents/clip-vit-large-patch14"
 
-anole_7b_v0_1_path = "/home/liying/Documents/Anole-7b-v0.1"
+# anole_7b_v0_1_path = "/home/liying/Documents/Anole-7b-v0.1"
+anole_7b_v0_1_path = "/home1/cjl/models/anole-7b"
 volcano_7b_path = "/home/liying/Documents/volcano-7b"
